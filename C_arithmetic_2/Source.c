bf8ff9ba28c642e70a1421696f5cdd67712cf89a
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-09 17:53:17 +0200

    Update Source.c

--- C_arithmetic_2/Source.c
+++ C_arithmetic_2/Source.c
@@ -9,7 +9,7 @@
 		of a circle.
 
 		Validation for the input is managed trough a
-		while loop. After the calculations the formatted
+		while loop. After the calculations a formatted
 		output is printed on the screen
 
 		*/
@@ -18,45 +18,45 @@
 
 
 
-#include <stdio.h>														// including the stdio header file
+#include <stdio.h>										// including the stdio header file
 
 
-const float Pi = 3.14159;												// a constnd float to hold Pi
+const float Pi = 3.14159;									// a constnd float to hold Pi
 
 
 int main()
 {
 
 		
-	float radius;														// a float to hold the radius
-	float circumference;												// a float to hold the circumference
-	float area;															// a float to hold the area
-	float diameter;														// a float to hold the diameter
+	float radius;										// a float to hold the radius
+	float circumference;									// a float to hold the circumference
+	float area;										// a float to hold the area
+	float diameter;										// a float to hold the diameter
 
 
 
 	printf("\nEnter the radius of the circle> ");						// prompting the user for an input
 	scanf_s("%f", &radius);
 
-	while (radius < 0.0)												// validating the input
+	while (radius < 0.0)									// validating the input
 	{
 		printf("\nInvalid input. Try again> ");
 		scanf_s("%f", &radius);
 	}
 
 
-	diameter = 2 * radius;												// calculating the diameter
-	circumference = Pi * diameter;										// calculating the circumference
-	area = Pi * (radius * radius);										// calcualting the area
+	diameter = 2 * radius;									// calculating the diameter
+	circumference = Pi * diameter;								// calculating the circumference
+	area = Pi * (radius * radius);								// calcualting the area
 
 
-	printf("\nDiameter: %15.2f", diameter);								// printing formatted output for the diameter
+	printf("\nDiameter: %15.2f", diameter);							// printing formatted output for the diameter
 	printf("\nCircumference: %10.2f", circumference);					// printing formatted output for the circumference
-	printf("\nArea: %20.2f", area);										// printing formatted output for the area
+	printf("\nArea: %20.2f", area);								// printing formatted output for the area
 		
-	printf("\n\n\tEnd of the program!\n\n");							// printing a message to indicate the end of the program
+	printf("\n\n\tEnd of the program!\n\n");						// printing a message to indicate the end of the program
 
 
-	return 0;															// exciting the program
+	return 0;										// exciting the program
 
 }

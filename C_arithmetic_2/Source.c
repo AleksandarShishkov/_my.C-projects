06760fa560a232fa7e7dc47387948c86a53f42fc
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-09 17:52:17 +0200

    Add files via upload

---
+++ C_arithmetic_2/Source.c
@@ -0,0 +1,62 @@
+
+
+
+
+		/*
+
+		This C program takes an input from the user and
+		calculates the area, diameter and circumference
+		of a circle.
+
+		Validation for the input is managed trough a
+		while loop. After the calculations the formatted
+		output is printed on the screen
+
+		*/
+
+
+
+
+
+#include <stdio.h>														// including the stdio header file
+
+
+const float Pi = 3.14159;												// a constnd float to hold Pi
+
+
+int main()
+{
+
+		
+	float radius;														// a float to hold the radius
+	float circumference;												// a float to hold the circumference
+	float area;															// a float to hold the area
+	float diameter;														// a float to hold the diameter
+
+
+
+	printf("\nEnter the radius of the circle> ");						// prompting the user for an input
+	scanf_s("%f", &radius);
+
+	while (radius < 0.0)												// validating the input
+	{
+		printf("\nInvalid input. Try again> ");
+		scanf_s("%f", &radius);
+	}
+
+
+	diameter = 2 * radius;												// calculating the diameter
+	circumference = Pi * diameter;										// calculating the circumference
+	area = Pi * (radius * radius);										// calcualting the area
+
+
+	printf("\nDiameter: %15.2f", diameter);								// printing formatted output for the diameter
+	printf("\nCircumference: %10.2f", circumference);					// printing formatted output for the circumference
+	printf("\nArea: %20.2f", area);										// printing formatted output for the area
+		
+	printf("\n\n\tEnd of the program!\n\n");							// printing a message to indicate the end of the program
+
+
+	return 0;															// exciting the program
+
+}

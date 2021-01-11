4fc87ea7fa290b1bb9945e67c297b15dcc49df99
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-11 15:38:52 +0200

    Update Source.c

--- C_sentinel_repetitions_2/Source.c
+++ C_sentinel_repetitions_2/Source.c
@@ -25,35 +25,35 @@ int main()
 {
 
 	int acc_num;										// an int to holed the account number
-	float current_ballance;								// a float to hold the current ballance
+	float current_ballance;									// a float to hold the current ballance
 	float charges;										// a float to hold the amount of charges
 	float credits;										// a float to hold the amount of credits
 	float new_ballance;									// a float to hold the new ballance
 
 
-	printf("\n\nEnter the account number ");			// prompting the user to enter the
-	printf("or press -1 to quit> ");					// account number
+	printf("\n\nEnter the account number ");						// prompting the user to enter the
+	printf("or press -1 to quit> ");							// account number
 	scanf_s("%d", &acc_num);
 
-	while (acc_num < 0 && acc_num != -1)				// validating the input
+	while (acc_num < 0 && acc_num != -1)							// validating the input
 	{
 		printf("\nInvalid valie. Try again> ");
 		scanf_s("%d", &acc_num);
 	}
 
-	while (acc_num != -1)								// entering the sentinel-controlled loop
+	while (acc_num != -1)									// entering the sentinel-controlled loop
 	{
-		printf("\nEnter the beggining amount $ ");		// prompting the user to enter the
-		scanf_s("%f", &current_ballance);				// current ballance
+		printf("\nEnter the beggining amount $ ");					// prompting the user to enter the
+		scanf_s("%f", &current_ballance);						// current ballance
 
-		while (current_ballance < 0)					// validating the input
+		while (current_ballance < 0)							// validating the input
 		{
 			printf("\nInvalid input. Try again> $ ");
 			scanf_s("%.2f", &current_ballance);
 		}
 
-		printf("\nEnter the total amount of charges ");	// prompting the user to enter the
-		printf("for the month> $ ");					// amount of the charges
+		printf("\nEnter the total amount of charges ");					// prompting the user to enter the
+		printf("for the month> $ ");							// amount of the charges
 		scanf_s("%f", &charges);
 
 		while (charges < 0)								// validating the input
@@ -62,8 +62,8 @@ int main()
 			scanf_s("%.2f", &charges);
 		}
 
-		printf("\nEnter the total amount of credits ");	// prompting the user to enter
-		printf("for the month> $ ");					// the amount of the credits
+		printf("\nEnter the total amount of credits ");					// prompting the user to enter
+		printf("for the month> $ ");							// the amount of the credits
 		scanf_s("%f", &credits);
 
 		while (credits < 0)								// validating the input
@@ -72,23 +72,23 @@ int main()
 			scanf_s("%.2f", &credits);
 		}
 
-		new_ballance = (current_ballance +				// calculating the new ballance
+		new_ballance = (current_ballance +						// calculating the new ballance
 			charges) - credits;
 
 
-		printf("\n\nAccount: %d", acc_num);				// printing the formatted output
+		printf("\n\nAccount: %d", acc_num);						// printing the formatted output
 		printf("\nCredit limit: $%.2f", current_ballance);
 		printf("\nBallance: $%.2f", new_ballance);
 
-		if (new_ballance > current_ballance)			// validating the new ballance
-			printf("\nCredit limit exceeded\n");		// printing message if valid	
+		if (new_ballance > current_ballance)						// validating the new ballance
+			printf("\nCredit limit exceeded\n");					// printing message if valid	
 
 
-		printf("\n\nEnter the account number ");		// prompting the user to enter 
-		printf("or press -1 to quit> ");				// the next account number
+		printf("\n\nEnter the account number ");					// prompting the user to enter 
+		printf("or press -1 to quit> ");						// the next account number
 		scanf_s("%d", &acc_num);
 
-		while (acc_num < 0 && acc_num != -1)			// validating the input
+		while (acc_num < 0 && acc_num != -1)						// validating the input
 		{	
 			printf("\nInvalid valie. Try again> ");
 			scanf_s("%d", &acc_num);
@@ -96,8 +96,8 @@ int main()
 
 	}
 
-	printf("\nThe program`s ended!\n");					// printing message indicating
-														// that the program`s ended
+	printf("\nThe program`s ended!\n");							// printing message indicating
+												// that the program`s ended
 
 	return 0;
 }

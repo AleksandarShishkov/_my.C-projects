becf17c30c5c020809c523ad6646b9a0e60f1bc2
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-11 17:11:45 +0200

    Update Source.c

--- C_sentinel_repetitions_3/Source.c
+++ C_sentinel_repetitions_3/Source.c
@@ -7,7 +7,7 @@
 		 and uses sentinel control loop to calculate
 		 the largest and the smallest numbers entered.
 
-		 It also keeps running total of the numbers
+		 It also keeps running total for the numbers
 		 entered. It print`s the output on the screen.
 		*/
 
@@ -18,17 +18,17 @@
 int main()
 {
 
-	int num;												// an integer to hold the input
-	int min;												// an integer to hold the largest value
-	int max;												// an integer to hold the smallest value
+	int num;											// an integer to hold the input
+	int min;											// an integer to hold the largest value
+	int max;											// an integer to hold the smallest value
 	int total = 0;											// an integer to keep running count
 
 
-	printf("\nEnter an integer (-1 to quit)> ");			// prompting the user to enter an integer
+	printf("\nEnter an integer (-1 to quit)> ");							// prompting the user to enter an integer
 	scanf_s("%d", &num);
 
-	max = num;												// assigning the initial value to max
-	min = num;												// assigning the initial value to min
+	max = num;											// assigning the initial value to max
+	min = num;											// assigning the initial value to min
 
 
 	while (num != -1)										// entering the sentinel controlled loop
@@ -39,26 +39,26 @@ int main()
 		if (min > num)										// validating the min value
 			min = num;
 
-		total++;											// incrementing the total variable
+		total++;										// incrementing the total variable
 
-		printf("\nEnter an integer (-1 to quit)> ");		// prompting the user to enter the next
-		scanf_s("%d", &num);								// integer
+		printf("\nEnter an integer (-1 to quit)> ");						// prompting the user to enter the next
+		scanf_s("%d", &num);									// integer
 
 	}
 
 
-	printf("\nLargest number:        %d", max);				// printing a formatted output for the
-															// largest number
+	printf("\nLargest number:        %d", max);							// printing a formatted output for the
+													// largest number
 
-	printf("\nSmallest number:       %d", min);				// printing a formatted output for the
-															// smallest value
+	printf("\nSmallest number:       %d", min);							// printing a formatted output for the
+													// smallest value
 
-	printf("\nTotal numbers entered: %d", total);			// printing a formatted output for the
-															// total numbers enterd
+	printf("\nTotal numbers entered: %d", total);							// printing a formatted output for the
+													// total numbers enterd
 
 
-	printf("\n\nThe program has ended!\n");					// prrionting a message indicating the
-															// end of the program
+	printf("\n\nThe program has ended!\n");								// prrionting a message indicating the
+													// end of the program
 
 
 	return 0;

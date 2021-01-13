1b05db87c8df7fa2ce0c571cbcdf9b09c6c7a289
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-13 22:15:22 +0200

    Update Source_menu.c

--- C_menu_arithmetic/Source_menu.c
+++ C_menu_arithmetic/Source_menu.c
@@ -21,28 +21,28 @@
 
 
 
-#include <stdio.h>																	// including the stdio directory
+#include <stdio.h>												// including the stdio directory
 
 
 int main()
 {
 
 
-	int choice = 0;																	// an integer to hold the choice
-	int num;																		// an integer to hold the inputted value
-	int sum = 0;																	// an integer to hold the sum
-	int count = 0;																	// an integer to hold th running count
-	double avrg = 0;																// a double to hold the average
-	int smallest;																	// an integer to hold the smallest number
-	int largest;																	// an integer to hodl the largerst number
-	int product = 1;																// an integer to hold the product
-	int fact_num;																	// an integer to hold the value for the factoriel
+	int choice = 0;												// an integer to hold the choice
+	int num;												// an integer to hold the inputted value
+	int sum = 0;												// an integer to hold the sum
+	int count = 0;												// an integer to hold th running count
+	double avrg = 0;											// a double to hold the average
+	int smallest;												// an integer to hold the smallest number
+	int largest;												// an integer to hodl the largerst number
+	int product = 1;											// an integer to hold the product
+	int fact_num;												// an integer to hold the value for the factoriel
 
 
-	while (choice != 9)																// entering the while loop
+	while (choice != 9)											// entering the while loop
 	{
 
-		puts("\n\n\tSelect between the following: \n");								// printing the menu
+		puts("\n\n\tSelect between the following: \n");							// printing the menu
 		puts("\nType  '1'  to sum serie of integers");
 		puts("Type  '2'  to calculate the average of serie of integers");
 		puts("Type  '3'  to view the smallest in serie of integers");
@@ -53,30 +53,30 @@ int main()
 		puts("Type  '7'  to calculate the facotiral of an integer");
 		puts("Type  '9'  to exit the program");
 		printf("> ");
-		scanf_s("%d", &choice);														// prompting the user to enter the choice
+		scanf_s("%d", &choice);										// prompting the user to enter the choice
 
 
-		switch (choice)																// a switch statement for the choice
+		switch (choice)											// a switch statement for the choice
 		{
 
 		case 1:
 
 			puts("\n\tYou have selected 'sum of integers'\n");
 
-			printf("Enter an integer. To quit type '-1'> ");						// prompting the user to enter serie of integers
+			printf("Enter an integer. To quit type '-1'> ");					// prompting the user to enter serie of integers
 			scanf_s("%d", &num);
 
 
-			while (num != -1)														// entering the sentinel-controlled loop
+			while (num != -1)									// entering the sentinel-controlled loop
 			{
-				sum += num;															// summing the inputted integers
+				sum += num;									// summing the inputted integers
 
 				printf("Enter an integer. To quit type '-1'> ");
 				scanf_s("%d", &num);
 
 			}
 
-			printf("\nThe sum is: %d\n", sum);										// printing the result
+			printf("\nThe sum is: %d\n", sum);							// printing the result
 
 			break;
 
@@ -86,22 +86,22 @@ int main()
 			puts("\n\tYou have selected 'average of integers'\n");
 
 
-			printf("Enter an integer. To quit type '-1'> ");						// prompting the user to enter serie of integers
+			printf("Enter an integer. To quit type '-1'> ");					// prompting the user to enter serie of integers
 			scanf_s("%d", &num);
 
-			while (num != -1)														// entering the sentinel-controlled loop
+			while (num != -1)									// entering the sentinel-controlled loop
 			{
-				avrg += num;														// summing the onputted integers
-				count++;															// incrementing the count
+				avrg += num;									// summing the onputted integers
+				count++;									// incrementing the count
 
 
 				printf("Enter an integer. To quit type '-1'> ");
 				scanf_s("%d", &num);
 			}
 
-			avrg /= count;															// calculating the average
+			avrg /= count;										// calculating the average
 
-			printf("\nThe average is: %.2f\n", avrg);								// printing the result
+			printf("\nThe average is: %.2f\n", avrg);						// printing the result
 
 			break;
 
@@ -109,15 +109,15 @@ int main()
 
 			puts("\n\tYou have selected 'smallest in the serie'\n");
 
-			printf("Enter an integer. To quit type '-1'> ");						// prompting the user to enter serie of integers
+			printf("Enter an integer. To quit type '-1'> ");					// prompting the user to enter serie of integers
 			scanf_s("%d", &num);
 
-			smallest = num;															// assigning the first integer to the smallest variable
+			smallest = num;										// assigning the first integer to the smallest variable
 
-			while (num != -1)														// entering the sentinel-controlled lopp
+			while (num != -1)									// entering the sentinel-controlled lopp
 			{
-				if (smallest > num)													// validating the input
-					smallest = num;													// assigning the next inputted integer if valid
+				if (smallest > num)								// validating the input
+					smallest = num;								// assigning the next inputted integer if valid
 
 				printf("Enter an integer. To quit type '-1'> ");
 				scanf_s("%d", &num);
@@ -131,15 +131,15 @@ int main()
 
 			puts("\n\tYou have selected 'largest in the serie'\n");
 
-			printf("Enter an integer. To quit type '-1'> ");						// prompting the user to enter serie of integers
+			printf("Enter an integer. To quit type '-1'> ");					// prompting the user to enter serie of integers
 			scanf_s("%d", &num);
 
-			largest = num;															// assigning the first integer to the largest variable
+			largest = num;										// assigning the first integer to the largest variable
 
-			while (num != -1)														// entering the sentinel-controlled loop
+			while (num != -1)									// entering the sentinel-controlled loop
 			{
-				if (largest < num)													// validating the input
-					largest = num;													// assinging the next inputted integer if vald
+				if (largest < num)								// validating the input
+					largest = num;								// assinging the next inputted integer if vald
 
 				printf("Enter an integer. To quit type '-1'> ");
 				scanf_s("%d", &num);
@@ -155,19 +155,19 @@ int main()
 
 			puts("\n\tYou have selected 'sum of the even numbers in the serie'\n");
 
-			printf("Enter an integer. To quit type '-1'> ");						// prompting the user to enter serie of integers
+			printf("Enter an integer. To quit type '-1'> ");					// prompting the user to enter serie of integers
 			scanf_s("%d", &num);
 
-			while (num != -1)														// entering the sentinel-controlled loop
+			while (num != -1)									// entering the sentinel-controlled loop
 			{
-				if (num % 2 == 0)													// validating the input
-					sum += num;														// summing the integers if valid
+				if (num % 2 == 0)								// validating the input
+					sum += num;								// summing the integers if valid
 
 				printf("Enter an integer. To quit type '-1'> ");
 				scanf_s("%d", &num);
 			}
 
-			printf("\nThe sum of all of the even numbers is: %d\n", sum);			// printing the result
+			printf("\nThe sum of all of the even numbers is: %d\n", sum);				// printing the result
 				
 			break;
 
@@ -176,22 +176,22 @@ int main()
 
 			puts("\n\tYou have selected 'product of the odd numbers in the serie'\n");
 
-			printf("Enter an integer. To quit type '-1'> ");						// prompting the user to enter serie of integers
+			printf("Enter an integer. To quit type '-1'> ");					// prompting the user to enter serie of integers
 			scanf_s("%d", &num);
 
 
 
-			while (num != -1)														// entering the sentinel-controlled loop
+			while (num != -1)									// entering the sentinel-controlled loop
 			{
-				if (num % 2 != 0)													// valdiating the input
-					product *= num;													// multiplying the odd integers
+				if (num % 2 != 0)								// valdiating the input
+					product *= num;								// multiplying the odd integers
 
 				printf("Enter an integer. To quit type '-1'> ");
 				scanf_s("%d", &num);
 
 			}
 
-			printf("\nThe product of all of the odd numbers is: %d\n", product);	// printing the result
+			printf("\nThe product of all of the odd numbers is: %d\n", product);			// printing the result
 
 			break;
 
@@ -200,16 +200,16 @@ int main()
 
 			puts("\n\tYou have selected 'factorial of an integer'\n");
 
-			printf("Enter an integer> ");											// prompting the user to enter an integer
+			printf("Enter an integer> ");								// prompting the user to enter an integer
 			scanf_s("%d", &fact_num);
 
-			int fact = 1;															// an integer to hold the facotiral
+			int fact = 1;										// an integer to hold the facotiral
 
-			if (fact_num < 0)														// validating the input
+			if (fact_num < 0)									// validating the input
 				printf("\nError. Invalid integer!");
 			else
 				for (int i = 1; i < fact_num; i++)
-					fact *= i;														// calculating the factorial
+					fact *= i;								// calculating the factorial
 
 			printf("\nThe factorial of %d is: %d\n", fact_num, fact);				// printing the result
 
@@ -218,12 +218,12 @@ int main()
 
 		case 9:
 
-			puts("\n\tThe program has terminated!\n");								// printing a message indicating that the
-																					// program has ended
+			puts("\n\tThe program has terminated!\n");						// printing a message indicating that the
+														// program has ended
 			break;
 
-		default:																	// a default case if invalid option is
-																					// selected
+		default:											// a default case if invalid option is
+														// selected
 			printf("\nInvalid input. Try again> \n");
 			break;
 

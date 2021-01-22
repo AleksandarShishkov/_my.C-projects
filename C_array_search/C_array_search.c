8f3017a1520345bd9e90c2c6d83cd2a6da782207
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-22 17:26:29 +0200

    Update C_array_search.c

--- C_array_search/C_array_search.c
+++ C_array_search/C_array_search.c
@@ -30,10 +30,10 @@ int main()
 {
 
 
-	int size;																// an integer to hold the size
-	int* arr = NULL;														// a pointer to an int
+	int size;														// an integer to hold the size
+	int* arr = NULL;													// a pointer to an int
 
-	char another_try;														// a char to hold the user`s choice
+	char another_try;													// a char to hold the user`s choice
 
 	bool try_again = true;													// a boolean to control the loop
 
@@ -41,17 +41,17 @@ int main()
 	while (try_again)
 	{
 
-		puts("\nEnter the size of the array> ");							// promting the user to enter
+		puts("\nEnter the size of the array> ");									// promting the user to enter
 		scanf_s("%d", &size);												// the size
 
-		while (size < 1)													// validating the input
+		while (size < 1)												// validating the input
 		{
 			printf("\n'%d' cannot be a valid size. Try again> ", size);
 			scanf_s("%d", &size);
 		}
 
-		arr = (int*)malloc(size, sizeof(int));								// allocating the memory for 
-																			// the array
+		arr = (int*)malloc(size, sizeof(int));										// allocating the memory for 
+																// the array
 
 		get_data(arr, size);												// calling get_data function
 		sort_arr(arr, size);												// calling sort_array function
@@ -60,11 +60,11 @@ int main()
 
 
 		getchar();
-		puts("\nAnother try (y/n)> ");										// promting the user to select
-		scanf_s("%c", &another_try);										// whether another array
-																			// will be created
+		puts("\nAnother try (y/n)> ");											// promting the user to select
+		scanf_s("%c", &another_try);											// whether another array
+																// will be created
 
-		while (tolower(another_try) != 'y' &&								// validating the input
+		while (tolower(another_try) != 'y' &&										// validating the input
 			tolower(another_try) != 'n')
 		{
 			printf("'%c' cannot be a valid input. Try again> ");
@@ -73,11 +73,11 @@ int main()
 
 		if (another_try == 'y')
 		{
-			try_again = true;												// assigning true to try_again	
+			try_again = true;											// assigning true to try_again	
 		}
 		else
 		{
-			try_again = false;												// assigning false to try_again
+			try_again = false;											// assigning false to try_again
 		}
 		
 		
@@ -86,27 +86,27 @@ int main()
 	}
 
 
-	puts("\nThe program has ended!\n");										// a message indicating that
-																			// the program has ended
+	puts("\nThe program has ended!\n");											// a message indicating that
+																// the program has ended
 
 	
-	free(arr);																// freeing the allocated memory
+	free(arr);														// freeing the allocated memory
 
 	return 0;
 }
 
 
-void get_data(int* arr, int size)											// a function definition for
-{																			// the elements in the array
+void get_data(int* arr, int size)												// a function definition for
+{																// the elements in the array
 
-	printf("\nEnter %d elements (1 - 100): \n", size);						// promting an input from the user
+	printf("\nEnter %d elements (1 - 100): \n", size);									// promting an input from the user
 
 	for (int i = 0; i < size; i++)
 	{
 		printf("Element #%d> ", i + 1);
 		scanf_s("%d", &*(arr + i));
 
-		while (*(arr + i) < 1 || *(arr + i) > 100)							// validating the input
+		while (*(arr + i) < 1 || *(arr + i) > 100)									// validating the input
 		{
 			printf("'%d' cannot be a valid value. Try again> ", *(arr + i));
 			scanf_s("%d", &*(arr + i));
@@ -114,16 +114,16 @@ void get_data(int* arr, int size)											// a function definition for
 	}
 }
 
-void sort_arr(int* arr, int size)											// a function definition for
-{																			// sorting the elements
+void sort_arr(int* arr, int size)												// a function definition for
+{																// sorting the elements
 
 	for (int i = 0; i < size - 1; i++)									
 	{
 		int min = *(arr + i);												// an integer to hold the current
-																			// value
+																// value
 
-		int index = i;														// an integer to hold the current
-																			// index
+		int index = i;													// an integer to hold the current
+																// index
 
 		for (int j = i + 1; j < size; j++)
 		{
@@ -134,23 +134,23 @@ void sort_arr(int* arr, int size)											// a function definition for
 			}
 		}
 
-		int temp = min;														// swapping the elements
+		int temp = min;													// swapping the elements
 		*(arr + index) = *(arr + i);
 		*(arr + i) = temp;
 	}
 
 }
 
-void search_arr(int* arr, int size)											// a function definition for
-{																			// searching for repeatable value
+void search_arr(int* arr, int size)												// a function definition for
+{																// searching for repeatable value
 
 	for (int i = 0; i < size; i++)
 	{
 		int num = *(arr + i);												// an integer to hold the current
-																			// element
+																// element
 
-		int dble_num = 0;													// an integer to keep count of
-																			// the repeatable elements
+		int dble_num = 0;												// an integer to keep count of
+																// the repeatable elements
 
 		for (int j = 0; j < size; j++)
 		{
@@ -158,7 +158,7 @@ void search_arr(int* arr, int size)											// a function definition for
 				dble_num += 1;
 		}
 
-		if (dble_num < 2)													// printing the element if
+		if (dble_num < 2)												// printing the element if
 			printf("%d ", *(arr + i));										// not repeated
 
 	}

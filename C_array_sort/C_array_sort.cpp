5168f0a98d83af7f04cde412fac86a9802901ee8
Aleksandar Shishkov <74405864+AleksandarShishkov@users.noreply.github.com>
2021-01-22 14:39:00 +0200

    Update C_array_sort.cpp

--- C_array_sort/C_array_sort.cpp
+++ C_array_sort/C_array_sort.cpp
@@ -15,40 +15,40 @@
 
 #include <stdio.h>
 
-#define SIZE 10														// defining global variable SIZE
+#define SIZE 10													// defining global variable SIZE
 
 void bubble_sort(int[]);											// a function definition for the
-																	// bubble sort with an argument for
-																	// the array
+														// bubble sort with an argument for
+														// the array
 
 void selection_sort(int[]);											// a function definition for the
-																	// selection sort with an argument
-																	// for the array
+														// selection sort with an argument
+														// for the array
 
 
 void get_data(int[]);												// a function definition for the 
-																	// user input with an argument
-																	// for the array
+														// user input with an argument
+														// for the array
 
 
 int main()
 {
 
-	int arr[SIZE];													// an array of integers
+	int arr[SIZE];												// an array of integers
 
 
-	get_data(arr);													// calling get_data function
+	get_data(arr);												// calling get_data function
 
 												
-	bubble_sort(arr);												// calling bubble_sort function
+	bubble_sort(arr);											// calling bubble_sort function
 	
 	
 	selection_sort(arr);											// calling selection_sort function
 
 
 
-	puts("\nThe program has ended!\n");								// a messsage indicating that
-																	// the program has ended
+	puts("\nThe program has ended!\n");									// a messsage indicating that
+														// the program has ended
 		
 
 	return 0;
@@ -59,8 +59,8 @@ void get_data(int arr[])											// function definition
 {
 	printf("\nEnter %d elements:\n", SIZE);
 
-	for (int i = 0; i < SIZE; i++)									// promting the user to enter the
-	{																// elements
+	for (int i = 0; i < SIZE; i++)										// promting the user to enter the
+	{													// elements
 		printf("Enter element #%d> ", i + 1);
 		scanf_s("%d", &arr[i]);
 	}
@@ -76,23 +76,23 @@ void get_data(int arr[])											// function definition
 void bubble_sort(int arr[])											// function definition
 {
 
-	bool swap;														// a boolean swap
-	int temp;														// an integer to hold the 
-																	// temporary value
+	bool swap;												// a boolean swap
+	int temp;												// an integer to hold the 
+														// temporary value
 
 	int num_iterations = 0;											// an integer to keep count of
-																	// the number of iterations
+														// the number of iterations
 
 	do
 	{
-		swap = false;												// setting swap to false
+		swap = false;											// setting swap to false
 
 		for (int i = 0; i < SIZE - 1; i++)
 		{
 			if (arr[i] > arr[i + 1])								// validating the current and the
-			{														// following element
+			{											// following element
 
-				temp = arr[i];										// swapping the values
+				temp = arr[i];									// swapping the values
 				arr[i] = arr[i + 1];
 				arr[i + 1] = temp;
 				swap = true;
@@ -105,33 +105,33 @@ void bubble_sort(int arr[])											// function definition
 
 	puts("\n\nBubble sort:");
 
-	for (int i = 0; i < SIZE; i++)									// printing the sorted array
+	for (int i = 0; i < SIZE; i++)										// printing the sorted array
 	{
 		printf("%d ", arr[i]);
 	}
 
-	printf("\nIterations: %d\n", num_iterations);					// printing the number of iterations
+	printf("\nIterations: %d\n", num_iterations);								// printing the number of iterations
 }
 
 
-void selection_sort(int arr[])										// a function definition
+void selection_sort(int arr[])											// a function definition
 {
 
 	int num_iterations = 0;											// an integer to hold the number
-																	// of iterations
+														// of iterations
 
 	for (int i = 0; i < SIZE - 1; i++)								
 	{
 		int minValue = arr[i];										// an integer to hold the current
-																	// element
+														// element
 
-		int index = i;												// an integer to hold the current
-																	// index
+		int index = i;											// an integer to hold the current
+														/ index
 
 		for (int j = i + 1; j < SIZE; j++)
 		{
 			if (arr[j] < minValue)									// validating the current element
-																	// the follwoing elements
+														// the follwoing elements
 			{
 				minValue = arr[j];
 				index = j;
@@ -148,13 +148,13 @@ void selection_sort(int arr[])										// a function definition
 
 	puts("\n\nSelection sort:");
 
-	for (int i = 0; i < SIZE; i++)									// printing the sorted array
+	for (int i = 0; i < SIZE; i++)										// printing the sorted array
 	{
 		printf("%d ", arr[i]);
 	}
 
-	printf("\nIterations: %d\n", num_iterations);					// printing the number of the
-																	// iterations
+	printf("\nIterations: %d\n", num_iterations);								// printing the number of the
+														// iterations
 
 }
 
